c674d97950858656385099b2ed2ab98f2d0f7b4a
Ambarish-Ai4x <ambarish.ganguly@tarento.com>
2025-11-20 18:34:49 +0530

    adding create service api

---
+++ services/model-management-service/models/service_create.py
@@ -0,0 +1,37 @@
+from typing import Dict, List, Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class BenchmarkEntry(BaseModel):
+    output_length: int
+    generated: int
+    actual: int
+    throughput: int
+    _50: int
+    _99: int
+    language: str
+
+    model_config = {
+        "populate_by_name": True,
+        "fields": {
+            "_50": "50%",
+            "_99": "99%",
+        },
+    }
+
+class ServiceStatus(BaseModel):
+    status: str
+    lastUpdated: str
+
+class ServiceCreateRequest(BaseModel):
+    serviceId: str
+    name: str
+    serviceDescription: str
+    hardwareDescription: str
+    publishedOn: int
+    modelId: str
+    endpoint: str
+    api_key: str
+    healthStatus: Optional[ServiceStatus] = None
+    benchmarks: Optional[Dict[str, List[BenchmarkEntry]]] = None
