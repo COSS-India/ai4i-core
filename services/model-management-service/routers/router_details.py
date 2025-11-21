0a93027d685f8e86c904c9a3ec75fe1e6ae2bfe7
Ambarish-Ai4x <ambarish.ganguly@tarento.com>
2025-11-21 21:17:58 +0530

    adding services apis

--- services/model-management-service/routers/router_details.py
+++ services/model-management-service/routers/router_details.py
@@ -1,6 +1,13 @@
 from fastapi import HTTPException, status , APIRouter
 from models.model_view import ModelViewRequest , ModelViewResponse
-from db_operations import get_model_details , list_all_models
+from models.service_view import ServiceViewRequest , ServiceViewResponse
+from models.service_list import ServiceListResponse
+from db_operations import (
+    get_model_details , 
+    list_all_models , 
+    get_service_details,
+    list_all_services
+)
 from logger import logger
 from typing import List
 
@@ -8,9 +15,11 @@ from typing import List
 router_details = APIRouter(prefix="/services/details", tags=["Model Management"])
 
 
+#################################################### Model apis ####################################################
+
 
 @router_details.post("/view_model", response_model=ModelViewResponse)
-async def view_model(payload: ModelViewRequest):
+async def view_model_request(payload: ModelViewRequest):
     
     try: 
         data = get_model_details(payload.modelId)
@@ -31,12 +40,12 @@ async def view_model(payload: ModelViewRequest):
 
 
 @router_details.get("/list_models" , response_model=List[ModelViewResponse])
-async def list_models():
+async def list_models_request():
     try:
         data = list_all_models()
 
         if not data:
-            return []  # Return empty list, not 404
+            return []
 
         return data
 
@@ -46,3 +55,45 @@ async def list_models():
             status_code=500,
             detail={"kind": "DBError", "message": "Error listing model details"}
         )
+    
+
+#################################################### Service apis ####################################################
+
+
+@router_details.post("/view_service", response_model=ServiceViewResponse)
+async def view_service_request(payload: ServiceViewRequest):
+    
+    try: 
+        data = get_service_details(payload.serviceId)
+
+        if not data:
+            raise HTTPException(status_code=404, detail="Service not found")
+        return data
+    
+    except HTTPException:
+        raise
+    except Exception:
+        logger.exception("Error while fetching service details from DB.")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={"kind": "DBError", "message": "Error fetching service details"}
+        )
+    
+
+@router_details.get("/list_services" , response_model=List[ServiceListResponse])
+async def list_services_request():
+    try:
+        data = list_all_services()
+
+        if not data:
+            return []
+
+        return data
+
+    except Exception:
+        logger.exception("Error while listing service details from DB.")
+        raise HTTPException(
+            status_code=500,
+            detail={"kind": "DBError", "message": "Error listing service details"}
+        )
+    
