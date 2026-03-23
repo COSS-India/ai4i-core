7661e88da55bd9cbf4e5dbfb681c272304178eaf
Ambarish <ambarish.ganguly@tarento.com>
2026-03-23 18:40:06 +0530

    tenant and user status transitions

--- services/multi-tenant-feature/models/enum_tenant.py
+++ services/multi-tenant-feature/models/enum_tenant.py
@@ -2,16 +2,14 @@ from enum import Enum
 
 class TenantStatus(str, Enum):
     PENDING = "PENDING"          # waiting for email verification 
-    IN_PROGRESS = "IN_PROGRESS"  
     ACTIVE = "ACTIVE"            # once email verified
     SUSPENDED = "SUSPENDED"
     DEACTIVATED = "DEACTIVATED"
-    # ARCHIVED = "ARCHIVED"
     
 class TenantUserStatus(str, Enum):
+    PENDING ="PENDING"           # used as default 
     ACTIVE = "ACTIVE"            # user created and approved by tenant admin
     SUSPENDED = "SUSPENDED"
-    DEACTIVATED = "DEACTIVATED"
 
 
 class TenantUserRole(str, Enum):
