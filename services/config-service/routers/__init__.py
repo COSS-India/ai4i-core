f3bf6cf667abbd3d5b2df99ae55d07d270c4f372
AleenaJayan <aleena.jayan@tarento.com>
2026-04-30 15:42:51 +0530

    fix: removed unleash from config service

--- services/config-service/routers/__init__.py
+++ services/config-service/routers/__init__.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 from .config_router import router as config_router
 from .service_registry_router import router as service_registry_router
 from .health_router import router as health_router
-from .feature_flag_router import router as feature_flag_router
 from .internal_health_router import router as internal_health_router
 
 __all__ = [
@@ -11,7 +10,6 @@ __all__ = [
     "config_router",
     "service_registry_router",
     "health_router",
-    "feature_flag_router",
     "internal_health_router",
 ]
 
