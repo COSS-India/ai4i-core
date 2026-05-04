688197af300e5df50daf3b803ca357693fe8de36
geojoseph <geojofficial@gmail.com>
2026-05-04 17:45:15 +0530

    chore: refactor

--- libs/ai4icore_env/ai4icore_env/settings.py
+++ libs/ai4icore_env/ai4icore_env/settings.py
@@ -403,5 +403,4 @@ class AppEnv(BaseSettings):
             return f"redis://:{self.redis_password}@{self.redis_host}:{self.redis_port}/{self.redis_db}"
         return f"redis://{self.redis_host}:{self.redis_port}/{self.redis_db}"
 
-
 app_env = AppEnv()
