7de40e445d365994db0497408bd01d0dd7859cf5
Vipul <vipuldholariya1991@gmail.com>
2026-05-27 16:24:38 +0530

    fix(migrations): resolve ai4iplatform_core multiple heads
    Chain the OCR adapter-config seed (f1a2b3c4d5e6) after the text seed (f7e8d9c0b1a2) instead of both branching off e4f6a8b2c1d0, giving a single linear head. validate-migrations now passes.

--- infrastructure/databases/migrations/postgres/alembic/versions/ai4iplatform_core/f1a2b3c4d5e6_seed_ocr_adapter_config.py
+++ infrastructure/databases/migrations/postgres/alembic/versions/ai4iplatform_core/f1a2b3c4d5e6_seed_ocr_adapter_config.py
@@ -1,7 +1,7 @@
 """seed_ocr_adapter_config
 
 Revision ID: f1a2b3c4d5e6
-Revises: e4f6a8b2c1d0
+Revises: f7e8d9c0b1a2
 Create Date: 2026-05-24 12:30:00.000000
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 
 revision: str = 'f1a2b3c4d5e6'
-down_revision: Union[str, None] = 'e4f6a8b2c1d0'
+down_revision: Union[str, None] = 'f7e8d9c0b1a2'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, None] = None
 
