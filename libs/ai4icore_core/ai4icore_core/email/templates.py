1ef4c08f0ae58299771a2cb91f281c5cba4aaebe
Supriya S V <supriya.vijaykumar@tarento.com>
2026-06-09 16:31:13 +0530

    Fixed PR comment

--- libs/ai4icore_core/ai4icore_core/email/templates.py
+++ libs/ai4icore_core/ai4icore_core/email/templates.py
@@ -29,7 +29,7 @@ class TemplateRenderer:
             autoescape=select_autoescape(default_for_string=True, default=True),
             **common,
         )
-        self._text_env = Environment(autoescape=True, **common)
+        self._text_env = Environment(autoescape=False, **common)
 
     def render(self, name: str, ctx: dict) -> tuple[str, str]:
         html = self._html_env.get_template(f"{name}.html").render(**ctx)
