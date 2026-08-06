f1fb3d6da78386b32659138995bab375737c859a
vishnu.unnikrishnan <vishnu.unnikrishnan@tarento.com>
2026-08-06 15:28:54 +0530

    Comments Fix

--- frontend/simple-ui/src/pages/model-management.tsx
+++ frontend/simple-ui/src/pages/model-management.tsx
@@ -343,7 +343,7 @@ const ModelManagementPage: React.FC = () => {
           createdOn: "2025-01-15T10:00:00.000Z",
           languages: {
             sourceLanguage: "hi",
-            targetLanguage: "hi",
+            targetLanguage: "en",
           },
           score: [
             {
