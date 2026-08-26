75fa68170d2356dfc27ea6adceae910f3acc02c8
AbhilashKTarento <abhilash.kumar@tarento.com>
2026-08-26 11:21:54 +0530

    TEMPORARY: added payperuse consumer argument temporarily

--- services/kafka-consumers/main.py
+++ services/kafka-consumers/main.py
@@ -1,4 +1,9 @@
+import sys
+
 from bootstrap.launcher import main
 
 if __name__ == "__main__":
+    # TODO: temporary default, remove once the caller always passes --consumer explicitly.
+    if len(sys.argv) == 1:
+        sys.argv += ["--consumer", "payperuse_consumer"]
     main()
