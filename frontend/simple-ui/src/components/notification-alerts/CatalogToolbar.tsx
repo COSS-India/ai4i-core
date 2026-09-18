fe58312ab7589e0db2663c5964ab8f0f1dec5da0
prekshavijay <preksha.vijay@tarento.com>
2026-09-18 19:47:19 +0530

    Table update- platform settings

--- frontend/simple-ui/src/components/notification-alerts/CatalogToolbar.tsx
+++ frontend/simple-ui/src/components/notification-alerts/CatalogToolbar.tsx
@@ -1,44 +1,6 @@
-import {
-  Box,
-  Checkbox,
-  FormControl,
-  HStack,
-  Input,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Checkbox, Text } from "@chakra-ui/react";
 import React from "react";
 import { RECIPIENT_ROLE_LABELS } from "../../types/notificationAlerts";
-
-interface CatalogToolbarProps {
-  search: string;
-  onSearchChange: (value: string) => void;
-  hint: string;
-}
-
-const CatalogToolbar: React.FC<CatalogToolbarProps> = ({
-  search,
-  onSearchChange,
-  hint,
-}) => {
-  return (
-    <Box mb={4}>
-      <HStack spacing={3} mb={3} align="stretch" flexWrap="wrap">
-        <FormControl maxW={{ base: "full", md: "280px" }}>
-          <Input
-            placeholder="Search by name"
-            value={search}
-            onChange={(e) => onSearchChange(e.target.value)}
-            bg="white"
-            size="md"
-          />
-        </FormControl>
-      </HStack>
-      <Text fontSize="sm" color="gray.600">
-        {hint}
-      </Text>
-    </Box>
-  );
-};
 
 interface RecipientRoleCheckboxesProps {
   tenantChecked: boolean;
@@ -73,5 +35,3 @@ export const RecipientRoleCheckboxes: React.FC<RecipientRoleCheckboxesProps> = (
     </Box>
   );
 };
-
-export default CatalogToolbar;
